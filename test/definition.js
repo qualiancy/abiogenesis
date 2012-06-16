822176802cb7facf056e470c62a93192224ae8f3
Jake Luer <jake.luer@gmail.com>
2012-06-16 00:48:28 -0400

    defintion now specified type of extension

--- test/definition.js
+++ test/definition.js
@@ -8,35 +8,54 @@ var Definition = abiogenesis.Definition;
 describe('Definition', function () {
 
   it('can be constructed', function () {
-    var def = new Definition('test', 'tester');
-    def.should.have.deep.property('_def.name', 'tester');
-    def.should.have.deep.property('_def.type', 'test');
+    var def = new Definition('test');
+    def.should.have.deep.property('_opts.name', 'test');
+    def.should.have.deep.property('_opts.type', 'definition');
     def.should.have.property('_requires').an('array');
+    def.should.have.property('_context', null);
+  });
+
+  it('can be extended', function () {
+    var Task = Definition.extend('task', {
+        action: function () {
+          this.__action = true;
+        }
+    });
+
+    console.log(Task.prototype);
+    var def = new Task('build');
+    def.should.have.deep.property('_opts.name', 'build');
+    def.should.have.deep.property('_opts.type', 'task');
+    def.should.have.property('_requires').an('array');
+    def.should.have.property('_context', null);
+    def.should.respondTo('action');
+    def.action();
+    def.should.have.property('__action', true);
   });
 
   describe('requirements', function () {
 
     it('can be defined singularly', function () {
-      var def = new Definition('test', 'tester')
-        , req = new Definition('test', 'required');
+      var def = new Definition('test 1')
+        , req = new Definition('test 2');
       def.should.respondTo('requires');
       def.requires(req);
       def._requires.should.include(req);
     });
 
     it('can be defined as an array', function () {
-      var def = new Definition('test', 'one')
-        , req1 = new Definition('test', 'req1')
-        , req2 = new Definition('test', 'req2');
+      var def = new Definition('one')
+        , req1 = new Definition('req1')
+        , req2 = new Definition('req2');
       def.should.respondTo('requires');
       def.requires([ req1, req2 ]);
       def._requires.should.include(req1, req2);
     });
 
     it('can be defined as arguments', function () {
-      var def = new Definition('test', 'one')
-        , req1 = new Definition('test', 'req1')
-        , req2 = new Definition('test', 'req2');
+      var def = new Definition('one')
+        , req1 = new Definition('req1')
+        , req2 = new Definition('req2');
       def.should.respondTo('requires');
       def.requires(req1, req2);
       def._requires.should.include(req1, req2);
