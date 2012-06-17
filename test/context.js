1b65453bef5cdbd90d453b8a5b49582afcb85a53
Jake Luer <jake.luer@gmail.com>
2012-06-16 20:46:06 -0400

    add context constructor + tests

---
+++ test/context.js
@@ -0,0 +1,171 @@
+var chai = require('chai')
+  , chaiSpies = require('chai-spies')
+  , should = chai.should();
+
+chai.use(chaiSpies);
+
+var abiogenesis = require('..')
+  , Context = abiogenesis.Context
+  , Definition = abiogenesis.Definition
+  , Runner = abiogenesis.Runner;
+
+describe('Context', function () {
+
+  describe('contruction', function () {
+
+    it('can be constructed', function () {
+      var base = new Context('base');
+      base.should.have.deep.property('_opts.name', 'base');
+      base.should.have.deep.property('_opts.type', 'context');
+      base.should.have.property('_runner', null);
+      base.should.have.property('_contexts').an('array');
+      base.should.have.property('_definitions').an('array');
+    });
+
+    it('can be extended', function () {
+      var Base = Context.extend('base')
+        , base = new Base('extended');
+      base.should.have.deep.property('_opts.name', 'extended');
+      base.should.have.deep.property('_opts.type', 'base');
+      base.should.have.property('_runner', null);
+      base.should.have.property('_contexts').an('array');
+      base.should.have.property('_definitions').an('array');
+    });
+
+    it('has an id', function () {
+      var base = new Context('base');
+      base.should.have.property('id', '/context/base');
+      var Suite = Context.extend('suite')
+        , suite = new Suite('base');
+      suite.should.have.property('id', '/suite/base');
+    });
+
+  });
+
+  describe('without runner', function () {
+    integration(false);
+  });
+
+  describe('with runner', function () {
+    integration(true);
+
+    describe('running', function () {
+
+      it('can run a single context', function (done) {
+        var runner = new Runner()
+          , base = new Context('base', { runner: runner })
+          , def1 = new Definition('thing 1')
+          , def2 = new Definition('thing 2');
+
+        var iterator = chai.spy(function (def, next) {
+          next();
+        });
+
+        runner.useDefinition(Definition, iterator);
+        runner.addContext(base);
+
+        base.addDefinition(def1);
+        base.addDefinition(def2);
+
+        runner.runContext('base', function (err) {
+          should.not.exist(err);
+          done();
+        });
+      });
+
+    });
+  });
+
+});
+
+function integration (useRunner) {
+
+  it('can nest indefinitely', function () {
+    Context.should.respondTo('addContext');
+    var runner = useRunner ? new Runner() : null
+      , base = new Context('base', { runner: runner })
+      , suite = new Context('suite')
+      , subsuite = new Context('subsuite')
+      , subsuite2 = new Context('subsuite2');
+
+    base.addContext(suite);
+    base._contexts.should.include(suite);
+    suite._parent.should.deep.equal(base);
+
+    suite.addContext(subsuite);
+    suite._contexts.should.include(subsuite);
+    subsuite._parent.should.deep.equal(suite);
+
+    suite.addContext(subsuite2);
+    suite._contexts.should.include(subsuite2);
+    subsuite2._parent.should.deep.equal(suite);
+
+    if (runner) {
+      runner._contexts.should.include(base);
+      runner._contexts.should.include(suite);
+      runner._contexts.should.include(subsuite);
+      runner._contexts.should.include(subsuite2);
+      subsuite2.runner.should.deep.equal(runner);
+    }
+  });
+
+  it('can add definitions', function () {
+    Context.should.respondTo('addDefinition');
+    var runner = useRunner ? new Runner() : null
+      , definition = new Definition('test')
+      , base = new Context('base', { runner: runner });
+
+    if (runner)
+      runner.useDefinition(Definition, function () {});
+
+    base.addDefinition(definition);
+    base._definitions.should.include(definition);
+
+    if (runner)
+      runner._definitions.should.include(definition);
+  });
+
+  if (!useRunner) {
+    it('can be pushed to a runner', function () {
+      var runner = new Runner()
+        , definition = new Definition('test')
+        , base = new Context('base')
+        , suite = new Context('suite');
+
+      runner.useDefinition(Definition, function () {});
+
+      base.addContext(suite);
+      suite.addDefinition(definition);
+
+      runner.pushContext(base);
+      runner._contexts.should.include(base, suite);
+      runner._definitions.should.include(definition);
+
+      var def = new Definition('test 2');
+      base.addDefinition(def);
+      runner._definitions.should.include(def);
+    });
+
+    it('can be assigned a runner', function () {
+      var runner = new Runner()
+        , definition = new Definition('test')
+        , base = new Context('base')
+        , suite = new Context('suite');
+
+      runner.useDefinition(Definition, function () {});
+
+      base.addContext(suite);
+      suite.addDefinition(definition);
+
+      suite.runner = runner;
+      base.runner.should.deep.equal(runner);
+      runner._contexts.should.include(base, suite);
+      runner._definitions.should.include(definition);
+
+      var def = new Definition('test 2');
+      base.addDefinition(def);
+      runner._definitions.should.include(def);
+    });
+  }
+
+}
